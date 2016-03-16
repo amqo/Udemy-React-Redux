7cbb3c3c5647f7de549833b0d0cfeeefca8da981
Alberto <alberto.commit@github.com>
2016-03-16 18:42:45 +0100

    Commit until class 30 Added video list and list items. Added callback for selected video.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = (props) => {
+const VideoList = ({videos, onVideoSelect}) => {
 
-  const videoItems = props.videos.map( video => {
-    return <VideoListItem key={video.etag} video={video} />
+  const videoItems = videos.map( video => {
+    return (
+      <VideoListItem
+        onVideoSelect={onVideoSelect}
+        key={video.etag}
+        video={video} />
+    );
   });
 
   return (
