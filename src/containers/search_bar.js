79a5e577130caf53765a70dc4a8845c3c252bc6b
Alberto <alberto.commit@github.com>
2016-03-21 13:45:40 +0100

    First commit for branch. Added search bar and submit button, inside new Redux container.

---
+++ src/containers/search_bar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <form action="" className="input-group">
+        <input type="text"/>
+        <span className="input-group-btn">
+          <button className="btn btn-secundary" type="submit">Submit</button>
+        </span>
+      </form>
+    );
+  }
+}
