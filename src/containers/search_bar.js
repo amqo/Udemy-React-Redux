02778a5d49be8a13bcd425d1edf76139dd578585
Alberto <alberto.commit@github.com>
2016-03-21 14:09:11 +0100

    Added component state to SearchBar. Make input state controlled.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({
+      term: event.target.value
+    });
+  }
+
   render() {
     return (
       <form action="" className="input-group">
-        <input type="text"/>
+        <input
+          placeholder="Get forecast in your favourite cities"
+          className="form-control"
+          onChange={ this.onInputChange }
+          type="text" value={ this.state.term }
+        />
         <span className="input-group-btn">
           <button className="btn btn-secundary" type="submit">Submit</button>
         </span>
